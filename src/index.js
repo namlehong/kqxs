4c0ae591d7a44a7308f221ab9749748be2302afd
Nam Le <namle@Nams-MacBook-Pro.local>
2018-03-09 10:52:45 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,62 @@
+// content of index.js
+const http = require('http')
+const app = require('express')()
+const port = 3000
+const request = require('request')
+const cheerio = require('cheerio')
+
+const MINGNGOC_URL = 'https://www.minhngoc.net.vn/tra-cuu-ket-qua-xo-so.html'
+
+const REGION_TO_ID = {
+  all: 0,
+  south: 1,
+  north: 2,
+  center: 3
+}
+
+function southCenterResultExtract($){
+  return $('.bkqmiennam table.rightcl').map((i, el) => {
+    return {
+      company: $(el).find('td').eq(0).text().replace(/[\n\t\r]/g,""),
+      code: $(el).find('td').eq(1).text().replace(/[\n\t\r]/g,""),
+      result: $(el).find('td div').map((i2, el2) => $(el2).text().replace(/[\n\t\r]/g,"")).get()
+    };
+  }).get();
+}
+
+function northResultExtract($){
+  return $('.bkqtinhmienbac').map((i, el) => {
+    return {
+      company: 'Hà Nội',
+      code: '',
+      result: $(el).find('tr').slice(1).find('td div').map((i2, el2) => $(el2).text().replace(/[\n\t\r]/g,"")).get().reverse()
+    };
+  }).get();
+}
+
+function resultHandler(body, req, res){
+  let $ = cheerio.load(body);
+
+  let result = [].concat(southCenterResultExtract($), northResultExtract($));
+
+  result.forEach(i => Object.assign(i, {date: req.params.year+'-'+req.params.month+'-'+req.params.day}));
+
+  // console.log('result', $('.bkqmiennam').html());
+  res.send(JSON.stringify(result));
+}
+
+app.get('/:region(all|north|center|south)/:year(\\d{4})-:month(\\d{2})-:day(\\d{2})', (req, res) => {
+  console.log(req.url, req.params)
+  let r = request({
+    url: MINGNGOC_URL,
+    qs: {
+      mien: REGION_TO_ID[req.params.region],
+      ngay: req.params.day,
+      thang: req.params.month,
+      nam: req.params.year
+    }
+  }, (error, response, body) => resultHandler(body, req, res));
+  // res.end('Hello Node.js Server!')
+});
+
+app.listen(port, () => console.log('Example app listening on port 3000!'))
